e549e5ac1c35b218af849f3cc13c06cc504782b2
Matt White <mwhite3194@gmail.com>
2016-11-10 13:02:44 -0800

    transitions
    added transitions

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $("#email").click(function(){
         $("#myModal").modal();
     });
+    $('#introduction').animate({'opacity':'1'},1800);
     $(window).scroll( function(){
         /* Check the location of each desired element */
         $('.hideme').each( function(i){
@@ -9,7 +10,8 @@ $(document).ready(function(){
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
+            //i used +400 for an offset to make it appear a little sooner so it appears AS you scroll
+            if( (bottom_of_window+400) > bottom_of_object ){
                 $(this).animate({'opacity':'1'},1000);
             }
         });
